49c7ad95cc20baaeb631795b6709643a96c5917b
Rafibintang22 <rafibintang26.rb@gmail.com>
2025-09-07 14:59:50 +0700

    update : new configuration

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,12 +1,32 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-app.whenReady().then(() => {
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({});
+// Create the browser window.
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+    });
 
     // path.join() digunakan untuk memastikan path dapat berjalan di windows, mac, dan linux
     // app.getAppPath() digunakan untuk mendapatkan path aplikasi electron
     // Load the React app
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        // Untuk macOS, karena umum untuk membuat window baru pada aplikasi ketika dock icon di klik dan dalam kondisi window tidak ada yg terbuka
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+
+    app.on("window-all-closed", () => {
+        // Untuk macOS, aplikasi dan menu bar tetap aktif sampai user quit secara eksplisit dengan Cmd + Q
+        // eslint-disable-next-line no-undef
+        if (process.platform !== "darwin") {
+            app.quit();
+        }
+    });
 });
